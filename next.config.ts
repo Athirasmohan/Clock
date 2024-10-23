b8f68802dff6bb8ac51938aee24d71d6f14b25d3
athiraasm@gmail.com <athiraasm@gmail.com>
2024-10-23 18:12:36 +0400

    second commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: '/Clock', // Replace 'Clock' with the name of your repository
+  assetPrefix: '/Clock/',
 };
 
 export default nextConfig;
